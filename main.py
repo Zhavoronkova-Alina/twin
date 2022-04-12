13786a49530984b6b88f0e5a630ccd75371001c9
Zhavoronkova-Alina <alina010299@mail.ru>
2022-04-12 21:22:31 +0300

    Rename notation, change some function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from mpmath import *
 from twin import *
-from intvalpy import Interval, precision
+from intvalpy import *
 
 
 def print_hi(name):
@@ -12,26 +12,27 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    a1 = 1
+    a1 = None
     a2 = 4
     b1 = 6
     b2 = 10
 
-    data1 = Interval(a1, a2)
-    data2 = Interval(b1, b2)
-    data3 = Interval(a1, b1)
-    data4 = Interval(a2, b2)
+    data1 = Interval(4, 6)
+    data2 = Interval(1, 10)
+    data3 = Interval(1, 4)
+    data4 = Interval(1, 10)
 
     tw1 = Twin(data1, data2)
-    tw1.print()
+    print(tw1)
     tw2 = Twin(data3, data4)
-    tw2.print()
+    print(tw2)
 
-    twins_plus(tw1, tw2).print()
+    print(twins_plus(tw1, tw2))
+    #twins_plus(tw1, tw2).print()
 
-    twins_multiply(tw1, tw2).print()
+    #print(twins_multiply(tw1, tw2))
 
-    unary_minus_twin(tw1).print()
+    #print(unary_minus_twin(tw1))
 
-    inverse_twin(tw1).print()
+    #print(inverse_twin(tw1))
 
--- twin.py
+++ twin.py
@@ -3,74 +3,72 @@ from intvalpy import Interval, precision, intersection
 
 
 class Twin(object):
-    def __init__(self, a1, a2):
-        # a1 and a2 must be a single intervals. If it have property "len", there are not single intervals
-        if hasattr(a1, 'len') or hasattr(a2, 'len'):
-            print("a1 or a2 is not single intervals. Check your data.")
+    def __init__(self, X_l, X):
+        # I1 and I2 must be a single intervals. If it have property "len", there are not single intervals
+        if hasattr(X_l, 'len') or hasattr(X, 'len'):
+            print("I1 or I2 is not single intervals. Check your data.")
             exit()
 
-        if isnan(float(a2.a)) or isnan(float(a2.b)):
+        if isnan(float(X.a)) or isnan(float(X.b)):
             print("The outer interval cannot be empty.")
             exit()
 
-        self.int = a1
+        self.X_l = X_l
 
-        if (isnan(float(a1.a)) or isnan(float(a1.b))) and a2.a == a2.b:
-            self.int = a2
+        if (isnan(float(X_l.a)) or isnan(float(X_l.b))) and X.a == X.b:
+            self.X_l = X
 
-        self.out = a2
+        self.X = X
 
-    def int_width(self):
-        if isnan(self.int.a):
-            return -1
-        return self.int.wid
+        if isnan(self.X_l.a):
+            self.X_l_width = -1
+        else:
+            self.X_l_width = self.X_l.wid
 
-    def out_width(self):
-        return self.out.wid
+        self.X_width = self.X.wid
 
-    def print(self):
-        print("Twin([ ", self.int, ", ", self.out, " ])")
-
+    def __str__(self):
+        return "[" + str(self.X_l) + ", " + str(self.X) + "]"
 
 # T1 = ( [a-, a+], [A-, A+] )
 # T2 = ( [b-, b+], [B-, B+] )
 
 def p(T1: Twin, T2: Twin):
-    if T1.int_width() == -1 and T2.int_width() == -1:
-        return None
+    if T1.X_l_width == -1 and T2.X_l_width == -1:
+        return float('nan')
 
-    if T1.int_width() == -1:
-        return T2.int.a + T1.out.b
+    if T1.X_l_width == -1:
+        return T2.X_l.a + T1.X.b
 
-    if T2.int_width() == -1:
-        return T1.int.a + T2.out.b
+    if T2.X_l_width == -1:
+        return T1.X_l.a + T2.X.b
 
-    return min(T1.int.a + T2.out.b, T2.int.a + T1.out.b)
+    return min(T1.X_l.a + T2.X.b, T2.X_l.a + T1.X.b)
 
 
 def q(T1: Twin, T2: Twin):
-    if T1.int_width() == -1 and T2.int_width() == -1:
-        return None
+    if T1.X_l_width == -1 and T2.X_l_width == -1:
+        return float('nan')
 
-    if T1.int_width() == -1:
-        return T2.int.b + T1.out.a
+    if T1.X_l_width == -1:
+        return T2.X_l.b + T1.X.a
 
-    if T2.int_width() == -1:
-        return T1.int.b + T2.out.a
+    if T2.X_l_width == -1:
+        return T1.X_l.b + T2.X.a
 
-    return max(T1.int.b + T2.out.a, T2.int.b + T1.out.a)
+    return max(T1.X_l.b + T2.X.a, T2.X_l.b + T1.X.a)
 
 
 def twins_plus(T1: Twin, T2: Twin):
-    if T1.out_width() <= T2.int_width() or T2.out_width() <= T1.int_width():
+    if T1.X_width <= T2.X_l_width or T2.X_width <= T1.X_l_width:
         return Twin(
             Interval(p(T1, T2), q(T1, T2)),
-            Interval(T1.out.a + T2.out.a, T1.out.b + T2.out.b)
+            Interval(T1.X.a + T2.X.a, T1.X.b + T2.X.b)
         )
     else:
         return Twin(
-            Interval(None, None),
-            Interval(T1.out.a + T2.out.a, T1.out.b + T2.out.b)
+            Interval(float('nan'), float('nan')),
+            Interval(T1.X.a + T2.X.a, T1.X.b + T2.X.b)
         )
 
 
@@ -78,9 +76,9 @@ def phi(I1, I2):
     Z = intersection(I1, I2)
 
     if not (isnan(float(Z.a)) or isnan(float(Z.b))):
-        return Interval(None, None)
+        return Interval(float('nan'), float('nan'))
     elif not isnan(float(Z.a)) and (not isnan(float(Z.b))) and Z.a != Z.b:
-        return Interval(None, None)
+        return Interval(float('nan'), float('nan'))
     else:
         min_ = abs(I1.a - I2.a)
         min_a = I1.a
@@ -114,66 +112,66 @@ def psi(I1, I2):
 # T1 = ( [a-, a+], [A-, A+] )
 # T2 = ( [b-, b+], [B-, B+] )
 def twins_multiply(T1: Twin, T2: Twin):
-    if T1.int_width() == -1 and T2.int_width() == -1:
+    if T1.X_l_width == -1 and T2.X_l_width == -1:
         return Twin(
-            Interval(None, None),
-            Interval(T1.out.a, T1.out.b) * Interval(T2.out.a, T2.out.b)
+            Interval(float('nan'), float('nan')),
+            Interval(T1.X.a, T1.X.b) * Interval(T2.X.a, T2.X.b)
         )
 
-    if T1.int_width() == -1:
+    if T1.X_l_width == -1:
         return Twin(
             phi(
-                T2.int.a * Interval(T1.out.a, T1.out.b),
-                T2.int.b * Interval(T1.out.a, T1.out.b)
+                T2.X_l.a * Interval(T1.X.a, T1.X.b),
+                T2.X_l.b * Interval(T1.X.a, T1.X.b)
             ),
-            Interval(T1.out.a, T1.out.b) * Interval(T2.out.a, T2.out.b)
+            Interval(T1.X.a, T1.X.b) * Interval(T2.X.a, T2.X.b)
         )
 
-    if T2.int_width() == -1:
+    if T2.X_l_width == -1:
         return Twin(
             phi(
-                T1.int.a * Interval(T2.out.a, T2.out.b),
-                T1.int.b * Interval(T2.out.a, T2.out.b)
+                T1.X_l.a * Interval(T2.X.a, T2.X.b),
+                T1.X_l.b * Interval(T2.X.a, T2.X.b)
             ),
-            Interval(T1.out.a, T1.out.b) * Interval(T2.out.a, T2.out.b)
+            Interval(T1.X.a, T1.X.b) * Interval(T2.X.a, T2.X.b)
         )
 
     return Twin(
         psi(
             phi(
-                mpf(T1.int.a) * Interval(T2.out.a, T2.out.b),
-                mpf(T1.int.b) * Interval(T2.out.a, T2.out.b)
+                (T1.X_l.a) * Interval(T2.X.a, T2.X.b),
+                (T1.X_l.b) * Interval(T2.X.a, T2.X.b)
             ),
             phi(
-                T2.int.a * Interval(T1.out.a, T1.out.b),
-                T2.int.b * Interval(T1.out.a, T1.out.b)
+                T2.X_l.a * Interval(T1.X.a, T1.X.b),
+                T2.X_l.b * Interval(T1.X.a, T1.X.b)
             )
         ),
-        Interval(T1.out.a, T1.out.b) * Interval(T2.out.a, T2.out.b)
+        Interval(T1.X.a, T1.X.b) * Interval(T2.X.a, T2.X.b)
     )
 
 
 def unary_minus_twin(T: Twin):
-    return Twin(-T.int, -T.out)
+    return Twin(-T.X_l, -T.X)
 
 
 def inverse_twin(T: Twin):
-    if 0 in T.int or 0 in T.out:
+    if 0 in T.X_l or 0 in T.X:
         print("Cannot be divided into intervals containing 0.")
         exit()
 
-    return Twin(1 / T.int, 1 / T.out)
+    return Twin(1 / T.X_l, 1 / T.X)
 
 
 def is_twins_equal(T1: Twin, T2: Twin):
-    if T1.int.a == T2.int.a and T1.int.b == T2.int.b and T1.out.a == T2.out.a and T1.out.b == T2.out.b:
+    if T1.X_l.a == T2.X_l.a and T1.X_l.b == T2.X_l.b and T1.X.a == T2.X.a and T1.X.b == T2.X.b:
         return True
     else:
         return False
 
 
 def is_twins_leq(T1: Twin, T2: Twin):
-    if (T1.out in T2.out) and (T2.int in T1.int):
+    if (T1.X in T2.X) and (T2.X_l in T1.X_l):
         return True
     else:
         return False
