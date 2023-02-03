7681d4873b810da90dc660db1724e9efc5fbf9d0
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2023-02-03 14:05:32 +0300

    Add operation into class

--- main.py
+++ main.py
@@ -11,24 +11,16 @@ if __name__ == '__main__':
     data2 = Interval(1, 100)
     data3 = Interval(float('nan'), float('nan'))
     data4 = Interval(2, 6)
-
     T1 = Twin(data1, data2)
     print("T1 = ", T1)
-    # T2 = Twin(data3, data4)
-    # print("T2 = ", T2)
+    T2 = Twin(data1, data4)
+    print("T2 = ", T2)
 
-    # print("p(T1, T2) = ", p(T1, T2))
-    # print("q(T1, T2) = ", q(T1, T2))
+    print("T1 + T2 = ", T1 + T2)
+    print("T1 * T2 = ", T1 * T2)
+    print(-T2)
+    print(T1 == T1)
+    print(T1 == T2)
+    print(T1 <= T1)
+    print(T1 <= T2)
 
-    # print("I1 = ", data1)
-    # print("I2 = ", data2)
-    # print("phi(I1, I2) = ", phi(data1, data2))
-    # print("psi(I1, I2) = ", psi(data1, data2))
-
-    # print("T1 + T2 = ", twins_plus(T1, T2))
-
-    # print("T1 * T2 = ", twins_multiply(T1, T2))
-
-    print("-T1 = ", unary_minus_twin(T1))
-
-    print("1 / T1 = ", inverse_twin(T1))
--- twin.py
+++ twin.py
@@ -1,5 +1,41 @@
 from mpmath import *
+from intvalpy import Interval
+from intvalpy import intersection
+#import operation as op
 
+def phi(I1, I2):
+    Z = intersection(I1, I2)
+
+    if not (isnan(float(Z.a)) or isnan(float(Z.b))):
+        return Interval(float('nan'), float('nan'))
+
+    min_ = abs(I1.a - I2.a)
+    min_a = I1.a
+    min_b = I2.a
+
+    if abs(I1.b - I2.b) < min_:
+        min_ = abs(I1.b - I2.b)
+        min_a = I1.b
+        min_b = I2.b
+
+    if abs(I1.a - I2.b) < min_:
+        min_ = abs(I1.a - I2.b)
+        min_a = I1.a
+        min_b = I2.b
+
+    if abs(I1.b - I2.a) < min_:
+        min_ = abs(I1.b - I2.a)
+        min_a = I1.b
+        min_b = I2.a
+
+    return Interval(min_a, min_b)
+
+
+def psi(I1, I2):
+    return Interval(
+        min(I1.a, I1.b, I2.a, I2.b),
+        max(I1.a, I1.b, I2.a, I2.b)
+    )
 
 class Twin(object):
     def __init__(self, X_l, X):
@@ -28,3 +64,99 @@ class Twin(object):
 
     def __str__(self):
         return "[" + str(self.X_l) + ", " + str(self.X) + "]"
+
+    def p(self, other):
+        if self.X_l_width == -1 and other.X_l_width == -1:
+            return float('nan')
+
+        if self.X_l_width == -1:
+            return other.X_l.a + self.X.b
+
+        if other.X_l_width == -1:
+            return self.X_l.a + other.X.b
+
+        return min(self.X_l.a + other.X.b, other.X_l.a + self.X.b)
+
+    def q(self, other):
+        if self.X_l_width == -1 and other.X_l_width == -1:
+            return float('nan')
+
+        if self.X_l_width == -1:
+            return other.X_l.b + self.X.a
+
+        if other.X_l_width == -1:
+            return self.X_l.b + other.X.a
+
+        return max(self.X_l.b + other.X.a, other.X_l.b + self.X.a)
+
+    def __add__(self, other):
+        if self.X_width <= other.X_l_width or other.X_width <= self.X_l_width:
+            return type(other)(
+                Interval(self.p(other), self.q(other)),
+                Interval(self.X.a + other.X.a, self.X.b + other.X.b)
+            )
+        else:
+            return type(other)(
+                Interval(float('nan'), float('nan')),
+                Interval(self.X.a + other.X.a, self.X.b + other.X.b)
+            )
+
+    def __mul__(self, other):
+        if self.X_l_width == -1 and other.X_l_width == -1:
+            return Twin(
+                Interval(float('nan'), float('nan')),
+                Interval(self.X.a, self.X.b) * Interval(other.X.a, other.X.b)
+            )
+
+        if self.X_l_width == -1:
+            return Twin(
+                phi(
+                    other.X_l.a * Interval(self.X.a, self.X.b),
+                    other.X_l.b * Interval(self.X.a, self.X.b)
+                ),
+                Interval(self.X.a, self.X.b) * Interval(other.X.a, other.X.b)
+            )
+
+        if other.X_l_width == -1:
+            return Twin(
+                phi(
+                    self.X_l.a * Interval(other.X.a, other.X.b),
+                    self.X_l.b * Interval(other.X.a, other.X.b)
+                ),
+                Interval(self.X.a, self.X.b) * Interval(other.X.a, other.X.b)
+            )
+
+        return Twin(
+            psi(
+                phi(
+                    (self.X_l.a) * Interval(other.X.a, other.X.b),
+                    (self.X_l.b) * Interval(other.X.a, other.X.b)
+                ),
+                phi(
+                    other.X_l.a * Interval(self.X.a, self.X.b),
+                    other.X_l.b * Interval(self.X.a, self.X.b)
+                )
+            ),
+            Interval(self.X.a, self.X.b) * Interval(other.X.a, other.X.b))
+
+    def __neg__(self):
+        return Twin(-self.X_l, -self.X)
+
+    def __invert__(self):
+        if 0 in self.X_l or 0 in self.X:
+            print("ERROR:Cannot be divided into intervals containing 0.")
+            exit()
+
+        return Twin(1 / self.X_l, 1 / self.X)
+
+    def __eq__(self,other):
+        if self.X_l.a == other.X_l.a and self.X_l.b == other.X_l.b and self.X.a == other.X.a and self.X.b == other.X.b:
+            return True
+        else:
+            return False
+
+    def __le__(self,other):
+        if (self.X in other.X) and (other.X_l in self.X_l):
+            return True
+        else:
+            return False
